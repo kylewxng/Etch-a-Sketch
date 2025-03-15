8715f53f4dd720de32b48d045451159948013db7
kylewxng <kylewxng06@gmail.com>
2025-03-15 16:23:58 -0700

    finished project

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 const container = document.getElementById("container");
 let size = 16;
-
-for(let row = 0; row < size; row++) {
-    for(let col = 0; col < size; col++) {
-        const square = document.createElement("div");
-        square.classList.add("grid-square");
-        container.appendChild(square);
-    }
-}
-
 const btn = document.getElementById("newGrid");
 
+createGrid(size);
 btn.addEventListener("click", () => {
     size = parseInt(prompt("How many squares do you want per side for the new grid?"))
-    if(size > 100) {
-        throw new Error("Input is too big");
+    if(size > 100 || size <= 0) {
+        alert("Invalid input size!");
+        createGrid(0);
     }
-    console.log(size);
+    else {
+        createGrid(size);
+    }
 });
+
+function createGrid(size) {
+    container.innerHTML = "";
+    const squareSize = 640/size;
+
+    for(let row = 0; row < size; row++) {
+        for(let col = 0; col < size; col++) {
+            const square = document.createElement("div");
+            square.classList.add("grid-square");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            container.appendChild(square);
+        }
+    }
+}
